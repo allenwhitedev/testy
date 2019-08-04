9353ddc827bfb2664105294c51e08de9e738382c
aw <aw@aws-MacBook-Pro.local>
2019-08-04 12:00:59 -0400

    Add chromedriver comment

--- scripts/selenium.ts
+++ scripts/selenium.ts
@@ -1,7 +1,8 @@
 import * as driver from 'selenium-webdriver'
 import {Builder, By, Key, until} from 'selenium-webdriver'
 import chromeDriver from 'selenium-webdriver/chrome'
- 
+
+// chromedriver must be in PATH (https://chromedriver.chromium.org/)
 export function chrome(headless: boolean) {
 	const builder = new Builder().forBrowser('chrome')
 	const builderWithOpts = headless ? builder.setChromeOptions(new chromeDriver.Options().headless()) : builder;
